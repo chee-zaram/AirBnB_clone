c316283f9950e0256aab226cdf6f178da3e203c7
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-02-06 18:34:36 +0100

    feat(basemodel): update `BaseModel` to instantiate from a dict
    The dictionary should contain attribute names and values, and we use the
    values to set our instance attributes

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This is the ``base_model`` module
+"""This is the ``base_model`` module
 
 It contains the class `BaseModel`
 """
@@ -9,20 +9,49 @@ from datetime import datetime
 
 
 class BaseModel:
-    """ This is the class `BaseModel`.
+    """This is the class `BaseModel`
 
     It defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
-        """ Runs only once when a new instance is created
+    def __init__(self, *args, **kwargs):
+        """Runs only once when a new instance is created
 
         Initializes attributes for every instance of `BaseModel` or sub-classes
+
+        Args:
+            args (tuple): List of positional arguments (unused)
+            kwargs (dict): Key/Value pairs of attribute names and values
         """
 
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            # Here, if an attribute name we are interested is in kwargs, we use
+            # the value to set our instance attribute
+            # If it is not, we generate the value of our instance attribute
+
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+            if kwargs["id"]:
+                self.id = kwargs["id"]
+            else:
+                self.id = str(uuid4())
+
+            if kwargs["created_at"]:
+                date_string = kwargs["created_at"]
+                self.created_at = datetime.strptime(date_string, date_format)
+            else:
+                self.created_at = datetime.now()
+
+            if kwargs["updated_at"]:
+                date_string = kwargs["updated_at"]
+                self.updated_at = datetime.strptime(date_string, date_format)
+            else:
+                self.updated_at = datetime.now()
+
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Prints out a string representation of the class instance """
@@ -31,7 +60,7 @@ class BaseModel:
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """ Updates the public instance attribute `updated_at`
+        """Updates the public instance attribute `updated_at`
 
         This attributes is updated with the current datetime
         """
