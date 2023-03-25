aa71994b9b297f3e90c075b4e5dec160c07d8862
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-03-25 20:13:42 +0100

    feat(state): add database support for `State` class

--- models/state.py
+++ models/state.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 """This is the `state` module"""
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from models import storage_type
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """Inherits from `BaseModel`
 
     Attributes:
         name (str): Name of the State
     """
 
-    name = ""
+    __tablename__ = "states"
+    if storage_type == "db":
+        name = Column(String(60), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """Returns a list of cities in the state for file storage engine"""
+            from models.city import City
+            from models import storage
+
+            cities = storage.all(City).values()
+            return [city for city in cities if city.state_id == self.id]
