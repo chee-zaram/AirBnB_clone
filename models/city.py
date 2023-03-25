c9f8d0a14cf633c62d67bf33642bc5374b0f0c66
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-03-25 20:17:36 +0100

    feat(city): add database support for `City` class

--- models/city.py
+++ models/city.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """This is the `city` module"""
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """Inherits from `BaseModel` class
 
     Attributes:
@@ -12,5 +15,11 @@ class City(BaseModel):
         name (str): Name of the city
     """
 
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    if storage_type == "db":
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(60), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete, delete-orphan")
+    else:
+        state_id = name = ""
