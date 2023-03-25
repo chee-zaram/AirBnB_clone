dceb94a42aa92249989c7da1f74b9a920eb95d07
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-03-25 21:03:29 +0100

    fix(init): fix path to `db_storage` module

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ from os import getenv
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
 if storage_type == "db":
-    from models.engine.dbstorage import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
