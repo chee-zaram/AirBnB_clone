a0ae231411f7e14a772a4d1541b8ddf1a5f8a5da
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-05-02 14:51:03 +0100

    feat(api): enable endpoint `/places_search` for searching places
    Search can be based on any combination of states, cities, or amenities.

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,8 @@ from models import storage
 from models.place import Place
 from models.city import City
 from models.user import User
+from models.state import State
+from models.amenity import Amenity
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'])
@@ -101,3 +103,71 @@ def update_place(place_id):
 
     place.save()
     return make_response(jsonify(place.to_dict()), 200)
+
+
+@app_views.route('/places_search', methods=['POST'])
+def search_places():
+    """Search for places based on a given request"""
+
+    request_data = request.get_json(silent=True)
+    if request_data is None:
+        abort(400, "Not a JSON")
+
+    allowed = ("states", "cities", "amenities")
+    places = storage.all(Place).values()
+    cities = storage.all(City).values()
+    states = storage.all(State).values()
+
+    """ If request_data is empty dict or all its items are empty lists
+    """
+    if not request_data or all(
+            v == [] for k, v in request_data.items() if k in allowed):
+        return make_response(jsonify([
+            place.to_dict() for place in places
+        ]), 200)
+
+    city_ids = request_data.get("cities")
+    state_ids = request_data.get("states")
+    amenity_ids = request_data.get("amenities")
+
+    # If there are specific cities, get them
+    if city_ids:
+        rv = [city for city in cities if city.id in city_ids]
+    # If not specific cities and states aren't specified
+    elif not state_ids:
+        rv = [city for city in cities]
+        city_ids = [city.id for city in rv]
+    else:
+        rv = city_ids = []
+
+    if state_ids:
+        states = [state for state in states if state.id in state_ids]
+        [
+            rv.append(city) for state in states for city in state.cities
+            if city.id not in city_ids
+        ]
+
+        city_ids = [city.id for city in rv]
+
+    if amenity_ids:
+        rv = [
+            place for place in places
+            if place.city_id in city_ids and all(storage.get(
+                Amenity, amenity_id) in place.amenities
+                for amenity_id in amenity_ids)
+        ]
+    else:
+        rv = [place for place in places if place.city_id in city_ids]
+
+    reval = []
+    for am in rv:
+        """
+        If the source is db, there will be an `amenities` attribute which is a
+        list of all the `Amenity` objects, and they are not JSON serializable
+        so we remove the list
+        """
+        d = am.to_dict()
+        d.pop("amenities", None)
+        reval.append(d)
+
+    return make_response(jsonify(reval), 200)
